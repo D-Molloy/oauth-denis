a9d064247a28a61a2ec40879de03c82fb8fe1faa
Denis Molloy <denis.molloy@gmail.com>
2018-03-12 13:02:25 -0700

    google keys modularized and added

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -1,2 +1,13 @@
 const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20")
+const GoogleStrategy = require("passport-google-oauth20");
+const keys = require("./keys")
+passport.use(
+  new GoogleStrategy({
+    //options for google strategy
+    // need a client ID and client secret
+    clientID: keys.google.clientID,
+    clientSecret: keys.google.clientSecret
+  }), ()=>{
+      //passport CB function
+  }
+);
