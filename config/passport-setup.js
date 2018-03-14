817a0a7c9865c4a1c77be076695adb98ea1157b4
Denis Molloy <denis.molloy@gmail.com>
2018-03-13 22:21:58 -0700

    app finished

--- config/passport-setup.js
+++ config/passport-setup.js
@@ -38,7 +38,7 @@ passport.use(
       //this is where we exchange the code we get from google after consent and get the actual profile info
       //this is where we use mLab to check to see if the user exists in our db ...if they do, pull the exisitng info
       //if not create new profile in the db
-      //   console.log(profile);
+        // console.log(profile);
       //creating a user is asynchronous, so we want to wait until its finished
 
       //check if user exists in DB
@@ -46,17 +46,18 @@ passport.use(
       User.findOne({ googleId: profile.id }).then(currentUser => {
         if (currentUser) {
           //already a user
-          console.log("user is: ", currentUser);
+          // console.log("user is: ", currentUser);
             done(null, currentUser);  //goes to serializeUser
         } else {
           //if not create new user
           new User({
             userName: profile.displayName,
-            googleId: profile.id
+            googleId: profile.id,
+            thumbnail: profile._json.image.url
           })
             .save()
             .then(newUser => {
-              console.log("new user created: ", newUser);
+              // console.log("new user created: ", newUser);
               done(null, newUser); //goes to serializeUser
             });
         }
